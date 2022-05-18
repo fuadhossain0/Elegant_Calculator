fa4822572e994bbbfa77dcb2fc8f0546d0af708b
fuadhossain0 <103536382+fuadhossain0@users.noreply.github.com>
2022-05-18 06:29:42 +0600

    Class Added from Branch

---
+++ app/src/main/java/com/example/new_sample/GIT_Branch.java
@@ -0,0 +1,20 @@
+package com.example.new_sample;
+
+public class GIT_Branch {
+    private String name;
+
+    //Constructor
+    public GIT_Branch() {
+        name = "Shawon";
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
