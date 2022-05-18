556a18d49cd75ecf4728e03fa95f1eb2f1631030
fuadhossain0 <103536382+fuadhossain0@users.noreply.github.com>
2022-05-18 06:19:20 +0600

    Class Added

---
+++ app/src/main/java/com/example/new_sample/GIT_Assignment.java
@@ -0,0 +1,20 @@
+package com.example.new_sample;
+
+public class GIT_Assignment {
+    private String name;
+
+    //Constructor
+    public GIT_Assignment() {
+        name = "Shawon";
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
